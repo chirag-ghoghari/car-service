4c73cc8fdb8a52cb2ea20894c4cc8d005c60b73d
chirag-ghoghari <162360866+chirag-ghoghari@users.noreply.github.com>
2024-03-05 23:34:30 +0530

    cmt

--- index.php
+++ index.php
@@ -15,6 +15,10 @@ $row = mysqli_fetch_array($result);
 <!DOCTYPE html>
 <html lang="en">
 
+
+
+
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
